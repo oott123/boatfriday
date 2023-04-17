fcb8b7208ab86c49af287e66f652161b5c02fa0d
oott123 <git@public.oott123.com>
2023-04-17 21:49:12 +0800

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    root: true,
+    extends: require.resolve('@ainou/code-style'),
+    parserOptions: {
+      // tsconfigRootDir: __dirname,
+      // project: ['./tsconfig.eslint.json'],
+    }
+  }
